19a1f7d364bb21f878dc8932a70b9a08fa690749
Drysb <bobby.drysdale9@gmail.com>
2024-02-10 21:05:26 -0500

    Adding validity function

--- main.py
+++ main.py
@@ -25,3 +25,31 @@ def find_zero(board):
         return row_index, col_index  # Return indices of the first zero
   return None  # No zero found
 
+#Look to see if the placement of a number is valid
+def is_valid(board, row, col, value):
+  grid_row = (row // 3) + 1
+  grid_column = (col // 3) + 1
+  #Check for number in same row
+  for num in board[row]:
+    if num == value:
+      print(f"False")
+      return False
+  #Check for number in same column
+  for row in board:
+    if row[col] == value:
+      print(f"False")
+      return False
+  #Check 3x3 grid
+  for row_index, row1 in enumerate(board):
+    if ((row_index // 3) + 1) != grid_row:
+      continue
+    for col_index, num in enumerate(row1):
+      if ((col_index // 3) + 1) != grid_column:
+        continue
+      if num == value:
+        print(f"False")
+        return False
+  print(f"True")
+  return True
+
+
